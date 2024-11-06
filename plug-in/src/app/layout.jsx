fb705c6025705466e5172dbc00dfb5d5c55d51d7
Shalva59 <shakosmail1234567890@gmail.com>
2024-11-06 22:28:41 +0400

    slider problem

---
+++ plug-in/src/app/layout.jsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+import Header from "./components/Header/page";
+import Footer from "./components/Footer/page";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
